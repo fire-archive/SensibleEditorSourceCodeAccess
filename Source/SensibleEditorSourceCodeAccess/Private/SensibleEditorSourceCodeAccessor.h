653486c29e2ea56573e99dfbe9ad9897f9b46d63
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2014-10-28 19:10:52 -0700

    Update SensibleEditorSourceCodeAccessor.h

--- Source/SensibleEditorSourceCodeAccess/Private/SensibleEditorSourceCodeAccessor.h
+++ Source/SensibleEditorSourceCodeAccess/Private/SensibleEditorSourceCodeAccessor.h
@@ -23,7 +23,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "ISourceCodeAccessor.h"
 
-class FXCodeSourceCodeAccessor : public ISourceCodeAccessor
+class FSensibleSourceCodeAccessor : public ISourceCodeAccessor
 {
 public:
 	/** ISourceCodeAccessor implementation */
