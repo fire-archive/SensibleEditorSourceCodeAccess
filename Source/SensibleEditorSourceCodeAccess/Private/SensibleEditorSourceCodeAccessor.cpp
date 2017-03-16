86b12cd35009e38a45679279294c0086f2e5d613
Fernando Zhu <fernandoisadev@gmail.com>
2017-03-16 15:48:54 +1300

    Changed the header inlude order, otherwise it returns errors when recompiling UE4Editor (UnrealEngine 4.15.0)

--- Source/SensibleEditorSourceCodeAccess/Private/SensibleEditorSourceCodeAccessor.cpp
+++ Source/SensibleEditorSourceCodeAccess/Private/SensibleEditorSourceCodeAccessor.cpp
@@ -19,8 +19,8 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#include "SensibleEditorSourceCodeAccessPrivatePCH.h"
 #include "SensibleEditorSourceCodeAccessor.h"
+#include "SensibleEditorSourceCodeAccessPrivatePCH.h"
 #include "DesktopPlatformModule.h"
 
 #define LOCTEXT_NAMESPACE "SensibleEditorSourceCodeAccessor"
